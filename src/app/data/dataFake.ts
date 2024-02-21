fe2cc0828082ba33a3016ea823024b1b47b05d83
BrunoBu4l <bruno.kibruno@gmail.com>
2024-02-21 17:25:48 -0300

    segunda versão do Blog

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+export const dataFake = [
+
+  {
+    "id":"1",
+    "title": "HD wallpaper: Technology, Asus ROG, City",
+    "description": "Imagem futuristica sobre tecnologia.",
+    "photoCover":"https://c4.wallpaperflare.com/wallpaper/373/655/596/technology-asus-rog-city-hd-wallpaper-preview.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Placa-mãe",
+    "description": "Imagem de uma placa-mãe tecnologica que parece uma cidade.",
+    "photoCover":"https://c4.wallpaperflare.com/wallpaper/624/21/247/microsoft-windows-windows-10-technology-hi-tech-wallpaper-preview.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Códigos estilo hacker matrix",
+    "description": "imagem de cógigos hacker, excelente para lembrar matrix.",
+    "photoCover":"https://c4.wallpaperflare.com/wallpaper/204/67/597/technics-design-technology-wallpaper-preview.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Gamer!",
+    "description": "Setup gamer para gosta de luzes RGB.",
+    "photoCover":"https://c4.wallpaperflare.com/wallpaper/162/894/557/colorful-neon-computer-keyboards-wallpaper-preview.jpg"
+  }
+
+]
